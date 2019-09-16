4934131ad7952a8d74b979c81f9a18bd5dece8b0
Kevin Karma <kevinkarma55@gmail.com>
2019-09-16 00:11:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,21 +14,23 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
-    
-    if req.path.match(/cart/)
+    elsif req.path.match(/cart/)
       if @@cart.any?
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
       else 
-        resp.write "Your cart is empty"
+        "Your cart is empty"
       end 
     elsif req.path.match(/add/)
       add_item = req.params["item"]
-      add_item_if_exist(add_item)
+      if handle_search(add_item)
+        @@cart << add_item
+      else
+        "Item not found!"
+      end 
+    else
+      resp.write "Path Not Found"
     end
 
     resp.finish
@@ -39,17 +41,6 @@ class Application
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
-    end
-  end
-  def add_item_if_exist(search_term)
-    find_item = @@items.find do |item|
-      item == search_term
-    end 
-    if find_item
-      @@cart << find_item
-      return "Added #{find_item}"
-    else
-      return "We don't have that item"
     end
   end
 end
